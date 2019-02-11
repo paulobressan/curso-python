c1855c826c7b393d9a43d494ee371a9f516393a8
paulobressan <paulo.bressan@outlook.com>
2019-02-11 13:32:42 -0200

    Negando condição com not e criando notificação com and ou or

--- Python 3 parte 2: Avançando na linguagem/forca.py
+++ Python 3 parte 2: Avançando na linguagem/forca.py
@@ -3,4 +3,15 @@ def jogar():
     print('***BEM VINDO NO JOGO DE FORCA!***')
     print('*********************************')
 
+    palavra_secreta = 'banana'
+    enforcou = False
+    acertou = False
+
+    #enquanto não enforcou e não acertou
+    while not enforcou and not acertou:
+        print('jogando')
+
     print('FIM DE JOGO!')
+
+if __name__ == '__main__':
+    jogar()
