d3c8e1a38bbe7e67ccf850d811e5329baae5eb5a
paulobressan <paulo.bressan@outlook.com>
2019-02-11 22:25:34 -0200

    Utilizando negação e and em ifs, criando listas, manipulando listas, manipulando strings.

--- Python 3 parte 2: Avançando na linguagem/forca.py
+++ Python 3 parte 2: Avançando na linguagem/forca.py
@@ -4,14 +4,24 @@ def jogar():
     print('*********************************')
 
     palavra_secreta = 'banana'
+    letras_acertadas = ['_', '_', '_', '_', '_', '_']
     enforcou = False
     acertou = False
 
-    #enquanto não enforcou e não acertou
+    # enquanto não enforcou e não acertou
     while not enforcou and not acertou:
-        print('jogando')
-
+        # o str.lower converter a palavra para minuscula e o strip remove os espaço da frente e do final de uma palavra
+        chute = input('Qual letra? ').strip().lower()
+        print(chute)
+        index = 0
+        for letra in palavra_secreta:
+            if chute == letra.lower():
+                letras_acertadas[index] = letra
+                print(f'Encontrei a letra {chute} na posição {index}')
+            index += 1
+        print(letras_acertadas)
     print('FIM DE JOGO!')
+
 
 if __name__ == '__main__':
     jogar()
