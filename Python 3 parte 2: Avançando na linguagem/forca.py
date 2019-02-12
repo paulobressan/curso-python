65e67171a4a2f9ff7148fe9e4eca36c2ae54a05d
paulobressan <paulo.bressan@outlook.com>
2019-02-11 23:34:30 -0200

    Entendo o uso de list, tuple, set e dictionary

--- Python 3 parte 2: Avançando na linguagem/forca.py
+++ Python 3 parte 2: Avançando na linguagem/forca.py
@@ -3,7 +3,18 @@ def jogar():
     print('***BEM VINDO NO JOGO DE FORCA!***')
     print('*********************************')
 
+    # COLEÇÕES
+    # tuple('São Paulo','Rio de Janeiro') , tuple é uma coleção imutavel
+    # ['Paulo', 'Bruna'] , list é uma coleção mutavel
+    # {'Paulo':'Homem', 'Bruna': 'Mulher'} , dictionary é uma coleção mutavel e boa para atender o problema 
+    # que a list não resolve quando tenta busca um item de uma lista dentro de outra lista,
+    # exemplo lista = [('Paulo', 'Homem'), ('Bruna', 'Mulher')], não podemos realizar lista['Paulo'], portanto com
+    # o dictionary conseguimos colecao = {'Paulo':'Homem', 'Bruna': 'Mulher'}, colecao['Paulo'] resultado: 'Homem'
+    # Set, o set é utilizado quando temos uma coleção de dados que não pode se repetir, o set ignora a inserção
+    # de registro ja existente, portanto um set não contem indece.
+
     palavra_secreta = 'banana'
+    # Criando uma lista
     letras_acertadas = ['_', '_', '_', '_', '_', '_']
     enforcou = False
     acertou = False
