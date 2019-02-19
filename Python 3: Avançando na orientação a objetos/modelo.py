051fb23e4a2c72d97cf2f896b9ef968f93e27d14
paulobressan <paulo.bressan@outlook.com>
2019-02-18 22:17:09 -0300

    Utilizando herança e iniciando construtor da classe pai.

--- Python 3: Avançando na orientação a objetos/modelo.py
+++ Python 3: Avançando na orientação a objetos/modelo.py
@@ -1,35 +1,37 @@
-class Filme:
-    def __init__(self, nome, ano, duracao):
-        self.__nome = nome.title()
+class Programa:
+    def __init__(self, nome, ano):
+        self._nome = nome.title()
         self.ano = ano
-        self.duracao = duracao
-        self.__likes = 0
+        self._likes = 0
 
     @property
     def likes(self):
-        return self.__likes
+        return self._likes
 
     @property
     def nome(self):
-        return self.__nome
+        return self._nome
 
     @nome.setter
     def nome(self, nome):
-        self.__nome = nome
+        self._nome = nome
 
     def dar_like(self):
-        self.__likes += 1
+        self._likes += 1
 
 
-class Serie:
-    def __init__(self, nome, ano, temporada):
-        self.__nome = nome.title()
-        self.ano = ano
-        self.temporada = temporada
-        self.__likes = 0
+# Herdando da classe Programa
+class Filme(Programa):
+    # Utilizando o construtor da classe Pai
+    def __init__(self, nome, ano, duracao):
+        super().__init__(nome, ano)
+        self.duracao = duracao
 
-    def dar_like(self):
-        self.likes += 1
+
+class Serie(Programa):
+    def __init__(self, nome, ano, temporadas):
+        super().__init__(nome, ano)
+        self.temporadas = temporadas
 
 
 vingadores = Filme("Vingadores - Guerra infinita", 2018, 160)
